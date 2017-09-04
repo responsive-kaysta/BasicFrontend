ea76bdaa177ec6c7380de7c17979b0eaa8d7137e
Kay Stuckenschmidt <mailto.kaysta@gmail.com>
2017-09-04 18:05:13 +0200

    initial commit

---
+++ frontend.local/Scripts/Modules/SearchBox/searchbox.js
@@ -0,0 +1,18 @@
+﻿/* searchbox.js */
+
+$(document).ready(function () {
+
+    $("#searchQuerySubmit").prop('disabled', true);
+
+});
+
+$('input#searchQuery').keyup(function () {
+
+    if (($('#searchQuery').val().length >= 3)) {
+        $("#searchQuerySubmit").prop('disabled', false);
+    }
+    else {
+        $("#searchQuerySubmit").prop('disabled', true);
+    }
+
+});
---
+++ frontend.local/Scripts/Plugins/Tooltipster/tooltipster.js
@@ -0,0 +1,20 @@
+﻿/* tooltipster.js */
+
+$(document).ready(function () {
+
+    $('.tooltipster').tooltipster({
+        animation: 'fade',
+        delay: 200,
+        theme: 'tooltipster-punk',
+        contentAsHTML: true,
+        maxWidth: 560,
+        trigger: 'click',
+        interactive: true
+    });
+
+
+    $('ul.tooltip_ul li:even').addClass('even');
+
+    $('ul.tooltip_ul li:odd').addClass('odd');
+
+});
