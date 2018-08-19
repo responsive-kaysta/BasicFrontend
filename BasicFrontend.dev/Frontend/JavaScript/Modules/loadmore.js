275f415ee4d79bf53ee0cdbbedb0113fe7ea7dce
Kay Stuckenschmidt <responsive-it@outlook.com>
2018-08-19 17:58:53 +0200

    init commit

---
+++ BasicFrontend.dev/Frontend/JavaScript/Modules/loadmore.js
@@ -0,0 +1,19 @@
+﻿/* loadmore.js */
+
+$(document).ready(function () {
+
+    $("li.tooltipster").slice(0, 20).show();
+
+    $("#loadMore").on('click', function (e) {
+
+        e.preventDefault();
+
+        $("li:hidden").slice(0, 20).slideDown();
+
+        if ($("li:hidden").length == 0) {
+            $("#loadMore").fadeOut('slow');
+        }
+
+    });
+
+});
---
+++ BasicFrontend.dev/Frontend/JavaScript/global.es5.js
@@ -0,0 +1,47 @@
+﻿"use strict";
+
+(function ($) {
+
+    function createCookie(name, value, days) {
+        var expires;
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+            expires = "; expires=" + date.toGMTString();
+        } else {
+            expires = "";
+        }
+
+        document.cookie = name + "=" + value + expires + "; path=/";
+    }
+
+    function readCookie(name) {
+        var nameEq = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEq) == 0) return c.substring(nameEq.length, c.length);
+        }
+        return null;
+    }
+
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+    }
+
+    // read value from querystring
+    var vars = [],
+        hash;
+
+    var q = document.URL.split('?')[1];
+    if (q != undefined) {
+        q = q.split('&');
+        for (var i = 0; i < q.length; i++) {
+            hash = q[i].split('=');
+            vars.push(hash[1]);
+            vars[hash[0]] = hash[1];
+        }
+    }
+})(jQuery);
+
