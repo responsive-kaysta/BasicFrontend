07ce9a4dae091eb9271d994988af6f627a733bb2
Kay Stuckenschmidt <responsive-it@outlook.com>
2018-09-18 11:41:26 +0200

    chicken checkin

---
+++ BasicFrontend.dev/Scripts/utility.es5.js
@@ -0,0 +1,28 @@
+﻿
+// https://wp-mix.com/jquery-truncate-text/
+
+'use strict';
+
+function getElementEqualHeight(group) {
+    var tallest = 0;
+    group.each(function () {
+        var thisHeight = $(this).height();
+        if (thisHeight > tallest) {
+            tallest = thisHeight;
+        }
+    });
+    group.height(tallest);
+}
+
+function getTruncateString(group) {
+    if (group('#page-wrapper').width() < 1280) {
+        group('.target').each(function (index, value) {
+            $(this).html($(this).html().substring(0, 24));
+        });
+    } else {
+        group('.target').each(function (index, value) {
+            $(this).html($(this).html().substring(0, 48));
+        });
+    }
+}
+
