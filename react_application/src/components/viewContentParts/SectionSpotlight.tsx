2b6acd3cd07a7b224ab90848ef01b54e292c6df9
Kay Stuckenschmidt <responsive-it@outlook.com>
2019-02-16 21:19:43 +0100

    suspense

--- react_application/src/components/viewContentParts/SectionSpotlight.tsx
+++ react_application/src/components/viewContentParts/SectionSpotlight.tsx
@@ -3,19 +3,19 @@ import * as React from 'react';
 
 import ISectionSpotlight from '../../application/interfaces/viewContentParts/ISectionSpotlight';
 
-function SectionSpotlight({ header, paragraph, cssStyle = "transparent", image = ""}: ISectionSpotlight) {
+function SectionSpotlight({ header, paragraph, cssStyle = "transparent", image = "" }: ISectionSpotlight) {
     const style = "spotlight equal " + cssStyle;
     return (
         <>
-             <section className={style}>
-                    <div className="image">
-                        <img className="wide" src={image} alt="Brains and engineering" />
-                    </div>
-                    <div className="content">
-                        <h2>{header}</h2>
-                        <p>{paragraph}</p>
-                    </div>
-                </section>
+            <section className={style}>
+                <div className="image">
+                    <img className="wide" src={image} alt="Brains and engineering" />
+                </div>
+                <div className="content">
+                    <h2>{header}</h2>
+                    <p>{paragraph}</p>
+                </div>
+            </section>
         </>
     );
 }
--- react_application/src/components/views/contact/Contact.tsx
+++ react_application/src/components/views/contact/Contact.tsx
@@ -1,60 +1,40 @@
 
 import * as React from 'react';
+import { Suspense } from "react";
 
-import ContentHeader from '../../viewContentParts/ArticleBanner';
-import StateFullHello from '../../viewParts/StateFullHello';
+const SectionSpotlight = React.lazy(() => import('../../viewContentParts/SectionSpotlight'));
+const ArticleBanner = React.lazy(() => import('../../viewContentParts/ArticleBanner'));
 
 class Contact extends React.Component {
     public render() {
         return (
+            <>
+                <Suspense fallback={<div>Loading...</div>}>
 
-            <div>
-                <ContentHeader
-                    header="some contact text"
-                    paragraph="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
+                    <ArticleBanner header="contact text" paragraph="contact Lorem ipsum dolor sit amet, consectetur adipiscing elit." cssStyle="gray" />
+                    <SectionSpotlight header="spotlight" paragraph="spotlight Lorem ipsum dolor sit amet, consectetur adipiscing elit." cssStyle="light" image="/assets/images/spotlight_planing.png" />
 
-                <div className="light">
-
-                    <section className="spotlight equal">
-                        <div className="image">
-                            <img className="wide" src="/assets/images/spotlight_planing.png" alt="Brains and engineering" />
-                        </div>
-                        <div className="content">
-
-                            <StateFullHello languageName="Some TypeScript" enthusiasmLevel={3} />
-
-                        </div>
-                    </section>
-
-                    <section>
-                        <div className="grid-section">
-                            <div className="container">
-                                <div className="row clearfix">
-                                    <div className="col-md-12">
-
-                                        <div className="pahe-header">
-                                            <h2>some page-header welcome</h2>
-                                        </div>
-                                        <p className="lead">
-                                            Pellentesque placerat commodo libero, ut tincidunt lacus volutpat vel. Phasellus nec porta sem. Donec a nisi tincidunt, ornare ligula in, pharetra purus. Nulla cursus mauris id dolor luctus eleifend. Etiam non justo eleifend, sagittis ligula non, faucibus tellus. Fusce sagittis imperdiet magna a ornare. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat.
-                                    </p>
-                                        <p className="paragraph">
-                                            Duis eget lorem faucibus, ultrices enim eget, imperdiet lorem. Nam a rutrum odio. Donec commodo eros vel congue dapibus. Quisque pharetra justo leo, nec convallis leo suscipit vel. Fusce auctor sapien at nunc fringilla lacinia. Suspendisse id erat vel lorem mattis malesuada. Sed quam eros, ultrices id vestibulum rhoncus, tempor laoreet est. Phasellus orci nibh, consectetur in ante sit amet, maximus euismod sem.
-                                    </p>
-                                        <p className="paragraph">
-                                            Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc commodo laoreet turpis a ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed scelerisque nisi consequat nunc porttitor sagittis. Proin tempor venenatis quam non vehicula. Praesent interdum pulvinar dapibus. Nunc malesuada augue fermentum nunc auctor dapibus.
-                                    </p>
-
-                                    </div>
+                    <section className="light">
+                        <div className="container">
+                            <div className="row clearfix">
+                                <div className="pahe-header">
+                                    <h2>some page-header welcome</h2>
                                 </div>
+                                <p className="lead">
+                                    Pellentesque placerat commodo libero, ut tincidunt lacus volutpat vel. Phasellus nec porta sem. Donec a nisi tincidunt, ornare ligula in, pharetra purus. Nulla cursus mauris id dolor luctus eleifend. Etiam non justo eleifend, sagittis ligula non, faucibus tellus. Fusce sagittis imperdiet magna a ornare. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam erat volutpat.
+                                    </p>
+                                <p className="paragraph">
+                                    Duis eget lorem faucibus, ultrices enim eget, imperdiet lorem. Nam a rutrum odio. Donec commodo eros vel congue dapibus. Quisque pharetra justo leo, nec convallis leo suscipit vel. Fusce auctor sapien at nunc fringilla lacinia. Suspendisse id erat vel lorem mattis malesuada. Sed quam eros, ultrices id vestibulum rhoncus, tempor laoreet est. Phasellus orci nibh, consectetur in ante sit amet, maximus euismod sem.
+                                    </p>
+                                <p className="paragraph">
+                                    Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc commodo laoreet turpis a ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed scelerisque nisi consequat nunc porttitor sagittis. Proin tempor venenatis quam non vehicula. Praesent interdum pulvinar dapibus. Nunc malesuada augue fermentum nunc auctor dapibus.
+                                    </p>
                             </div>
                         </div>
                     </section>
 
-                </div>
-
-            </div>
-
+                </Suspense>
+            </>
         );
     }
 }
