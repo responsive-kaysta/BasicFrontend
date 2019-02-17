397125907b1c64e60ca04f6312a5c65ea0ca453a
Kay Stuckenschmidt <responsive-it@outlook.com>
2019-02-17 12:40:45 +0100

    mini format

--- react_application/src/components/ViewParts/StateFullHello.tsx
+++ react_application/src/components/ViewParts/StateFullHello.tsx
@@ -36,9 +36,7 @@ class StateFullHello extends React.Component<IStoreState, ICurrentState> {
 
 }
 
-
 export default StateFullHello;
-
 
 function getExclamationMarks(numChars: number) {
   return Array(numChars + 1).join('!');
