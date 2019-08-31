9681b5b1aa3b5920ea232b0133cbc894ddd5008a
Kay Stuckenschmidt <kay.stuckenschmidt@subsidia.ch>
2019-08-31 14:33:09 +0200

    form styling almost done

--- frontend_application/src/components/composition/PageAdminEditArchiveSource.tsx
+++ frontend_application/src/components/composition/PageAdminEditArchiveSource.tsx
@@ -38,7 +38,132 @@ class PageAdminEditArchiveSource extends React.Component<
                 &nbsp; Back
               </a>
             </div>
+
+            <div id="react_0HLPDV03N7M10">
+              <form>
+                <input
+                  id="sourceId"
+                  name="sourceId"
+                  value="75"
+                  type="hidden"
+                  readOnly
+                />
+                <input
+                  id="sourceVersion"
+                  name="sourceVersion"
+                  value="3"
+                  type="hidden"
+                  readOnly
+                />
+
+                <div className="flex-row-wrapper row">
+                  <label>Source Name</label>
+                  <input
+                    id="sourceName"
+                    name="sourceName"
+                    placeholder="Source Name"
+                    value="Tages-Anzeiger Börse"
+                    type="text"
+                    readOnly
+                  />
+                </div>
+
+                <div className="flex-row-wrapper row">
+                  <label>Source Description Text</label>
+                  <input
+                    id="sourceDescription"
+                    name="sourceDescription"
+                    placeholder="Source Description"
+                    value="Tages-Anzeiger Börse"
+                    type="text"
+                    readOnly
+                  />
+                </div>
+
+                <div className="flex-row-wrapper row">
+                  <label>Source Encoding</label>
+                  <input
+                    id="sourceEncoding"
+                    name="sourceEncoding"
+                    placeholder="Source Encoding"
+                    value="UTF-8"
+                    type="text"
+                    readOnly
+                  />
+                </div>
+
+                <div className="flex-row-wrapper row">
+                  <label>
+                    <a
+                      href="https://www.tagesanzeiger.ch/stock_market/uebersicht/rss.html"
+                      target="_blank"
+                    >
+                      <i className="fas fa-external-link-alt" />
+                      Source Link
+                    </a>
+                  </label>
+                  <input
+                    id="sourceLink"
+                    name="sourceLink"
+                    placeholder="Source Link"
+                    value="https://www.tagesanzeiger.ch/stock_market/uebersicht/rss.html"
+                    type="text"
+                    readOnly
+                  />
+                </div>
+
+                <div className="flex-row-wrapper row">
+                  <label>Source Type</label>
+                  <select>
+                    <option value="1">
+                      Realy Simple Syndication - Sehr Einfache Syndication
+                    </option>
+                    <option value="2">Web Link</option>
+                    <option value="3">Atom XML</option>
+                  </select>
+                </div>
+
+                <div className="flex-row-wrapper row">
+                  <label>Source Language</label>
+                  <select>
+                    <option value="1">Deutsch</option>
+                    <option value="2">English</option>
+                  </select>
+                </div>
+
+                <div className="flex-container row">
+                  <div className="flex-row-wrapper">
+                    <label>Source is online</label>
+                    <select>
+                      <option value="true">true</option>
+                      <option value="false">false</option>
+                    </select>
+                  </div>
+
+                  <div className="flex-row-wrapper">
+                    <label>Source is archived</label>
+                    <select>
+                      <option value="true">true</option>
+                      <option value="false">false</option>
+                    </select>
+                  </div>
+
+                  <div className="flex-row-wrapper">
+                    <label>Source is deleted</label>
+                    <select>
+                      <option value="true">true</option>
+                      <option value="false">false</option>
+                    </select>
+                  </div>
+                </div>
+
+                <div>
+                  <input value="Post" type="submit" />
+                </div>
+              </form>
+            </div>
           </section>
+
           <Footer />
         </React.Suspense>
       </>
