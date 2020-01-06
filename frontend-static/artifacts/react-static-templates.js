e954eada07bd329c3441d165f65ebd470b4104c1
Kay Stuckenschmidt <responsive-adm@outlook.com>
2020-01-06 20:19:40 +0100

    react-static template

---
+++ frontend-static/artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  '/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx': require('/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx').default,
+'/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/about.tsx': require('/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/about.tsx').default,
+'/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/blog.tsx': require('/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/blog.tsx').default,
+'/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/index.tsx': require('/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/index.tsx').default,
+'/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/containers/Post': require('/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/containers/Post').default
+}
+
+export const notFoundTemplate = '/home/kaysta/Dev/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx'
+
