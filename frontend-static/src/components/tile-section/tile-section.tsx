66bcf76b0433f9c7ab90c9334f67b558719767b3
Kay Stuckenschmidt <responsive-adm@outlook.com>
2020-01-07 22:30:46 +0100

    rename to tiles

--- frontend-static/src/components/tile-section/tile-section.tsx
+++ frontend-static/src/components/tile-section/tile-section.tsx
@@ -5,7 +5,7 @@ class TileSection extends React.Component<iTileSectionProps> {
   public render() {
     return (
       <section className={this.props.pageStyle}>
-        <section className="blogarchive">{this.props.children}</section>
+        <section className="tiles">{this.props.children}</section>
       </section>
     );
   }
