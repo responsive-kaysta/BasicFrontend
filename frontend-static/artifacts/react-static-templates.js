c7bca5c163a47e7f9b5375862a05fd38d1dd0dd8
Kay Stuckenschmidt <kay.stuckenschmidt@subsidia.ch>
2020-02-20 15:48:54 +0100

    color styles

--- frontend-static/artifacts/react-static-templates.js
+++ frontend-static/artifacts/react-static-templates.js
@@ -3,18 +3,18 @@
   
 // Template Map
 export default {
-  'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/403.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/403.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/box-panel.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/box-panel.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/container.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/container.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/features.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/features.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/index.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/index.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/link-button.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/link-button.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/list-icon.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/list-icon.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/spotlight.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/spotlight.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/text-heading.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/text-heading.tsx').default,
-'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/tiles-section.tsx': require('C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/tiles-section.tsx').default
+  '/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/403.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/403.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/box-panel.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/box-panel.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/container.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/container.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/features.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/features.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/index.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/index.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/link-button.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/link-button.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/list-icon.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/list-icon.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/spotlight.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/spotlight.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/text-heading.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/text-heading.tsx').default,
+'/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/tiles-section.tsx': require('/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/tiles-section.tsx').default
 }
 
-export const notFoundTemplate = 'C:/Users/respo/Source/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx'
+export const notFoundTemplate = '/home/kaysta/GitHub/BasicFrontend/frontend-static/src/pages/404.tsx'
 
