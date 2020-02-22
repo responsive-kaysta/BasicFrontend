c783f8504ebc24a958fdf0d471d4af76a5afd89d
Kay Stuckenschmidt <kay.stuckenschmidt@subsidia.ch>
2020-02-22 23:13:17 +0100

    google stuff

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         <link rel="icon" href="/icon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        <link rel="apple-touch-icon" href="/logo192.png" />
+        {/* <link rel="apple-touch-icon" href="/logo192.png" /> */}
         {/* <link rel="manifest" href="/manifest.json" /> */}
         <meta name="page-topic" content={PAGE_TOPIC} />
         <meta name="robots" content="all" />
