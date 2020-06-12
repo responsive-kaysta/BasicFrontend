2f5e68714e507dff3f278f983544f977e6778689
Kay Stuckenschmidt <kay.stuckenschmidt@subsidia.ch>
2020-06-12 15:30:00 +0200

    npm upgrade, security commit

--- packages/basic-frontend-storybook/.storybook/config.js
+++ packages/basic-frontend-storybook/.storybook/config.js
@@ -1,6 +1,25 @@
 import { configure } from "@storybook/react";
 const req = require.context("../src/stories", true, /.stories.tsx$/);
+
+export const parameters = {
+  options: {
+    showRoots: true,
+  },
+  // automatically create action args for all props that start with "on"
+  actions: { argTypesRegex: "^on.*" },
+  dependencies: {
+    // display only dependencies/dependents that have a story in storybook
+    // by default this is false
+    withStoriesOnly: true,
+
+    // completely hide a dependency/dependents block if it has no elements
+    // by default this is false
+    hideEmpty: true,
+  },
+};
+
 function loadStories() {
   req.keys().forEach(req);
 }
+
 configure(loadStories, module);
