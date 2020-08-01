d22405d243c6bdd51ad909724dfda108b66ba481
Kay Stuckenschmidt <kay.stuckenschmidt@subsidia.ch>
2020-08-01 17:45:00 +0200

    another try

--- packages/basic-frontend-common/webpack.config.js
+++ packages/basic-frontend-common/webpack.config.js
@@ -1,14 +1,3 @@
-const CopyPlugin = require("copy-webpack-plugin");
-
 module.exports = {
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: "./src/images/*.svg", to: "./build/images/" },
-        { from: "./src/images/*.png", to: "./build/images/" },
-        { from: "./src/images/*.jpg", to: "./build/images/" },
-        { from: "./src/pictograms/**/*.svg", to: "./build/pictograms/**/" },
-      ],
-    }),
-  ],
+  plugins: [],
 };
