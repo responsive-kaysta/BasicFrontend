337189f9d3595dd65b0d3b7d3120680e81062cc7
Kay Stuckenschmidt <responsive-adm@outlook.com>
2020-08-03 13:24:12 +0200

    svgs

--- packages/basic-frontend-storybook/src/stories/01-identity/04-pictograms.stories.tsx
+++ packages/basic-frontend-storybook/src/stories/01-identity/04-pictograms.stories.tsx
@@ -9,10 +9,13 @@ import {
 } from "basic-frontend-common";
 import React from "react";
 
-const pictos = allPictogramNames.filter((x) => !x.includes("ai_"));
+const pictos = allPictogramNames.filter(
+  (x) => !x.includes("ai_") && !x.includes("cloud_")
+);
 const artificial_intelligence = allPictogramNames.filter((x) =>
   x.includes("ai_")
 );
+const cloud = allPictogramNames.filter((x) => x.includes("cloud_"));
 
 storiesOf("Design System / 01 Identity", module).add("Pictograms", () => (
   <>
@@ -39,6 +42,25 @@ storiesOf("Design System / 01 Identity", module).add("Pictograms", () => (
       <TextLead>Artificial-Intelligence</TextLead>
       <ul className="flex flex-row flex-wrap mt-8">
         {artificial_intelligence.map((key) => (
+          <li
+            key={key}
+            className="flex flex-col items-center mb-8 mr-8 text-gray-900"
+          >
+            <Pictogram
+              name={key as PictogramName}
+              aria-label={key}
+              size={PictogramSize.sm}
+            />
+            <div className="mt-2">{key}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <Hruler />
+    <div>
+      <TextLead>Cloud and Network</TextLead>
+      <ul className="flex flex-row flex-wrap mt-8">
+        {cloud.map((key) => (
           <li
             key={key}
             className="flex flex-col items-center mb-8 mr-8 text-gray-900"
