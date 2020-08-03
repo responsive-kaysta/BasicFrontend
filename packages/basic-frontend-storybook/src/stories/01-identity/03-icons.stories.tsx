753f77f190a7b86209774454960b2dc7509c8b17
Kay Stuckenschmidt <responsive-adm@outlook.com>
2020-08-03 16:51:25 +0200

    icon set

--- packages/basic-frontend-storybook/src/stories/01-identity/03-icons.stories.tsx
+++ packages/basic-frontend-storybook/src/stories/01-identity/03-icons.stories.tsx
@@ -1,18 +1,23 @@
 import { storiesOf } from "@storybook/react";
-import { allIconNames, Icon, IconName, IconSize } from "basic-frontend-common";
+import {
+  allIconNames,
+  Icon,
+  IconName,
+  IconSize,
+  TextLead,
+} from "basic-frontend-common";
 import React from "react";
 
 storiesOf("Design System / 01 Identity", module).add("Icons", () => (
   <div>
-    <ul className="flex flex-row flex-wrap">
+    <TextLead>Artificial-Intelligence</TextLead>
+    <ul className="flex flex-row flex-wrap mt-8">
       {allIconNames.map((key) => (
-        <li key={key} className="flex flex-col items-center mb-8 mr-8">
-          <Icon
-            name={key as IconName}
-            aria-label={key}
-            size={IconSize.sm}
-            className=""
-          />
+        <li
+          key={key}
+          className="flex flex-col items-center mb-8 mr-8 text-gray-900"
+        >
+          <Icon name={key as IconName} aria-label={key} size={IconSize.sm} />
           <div className="mt-2">{key}</div>
         </li>
       ))}
