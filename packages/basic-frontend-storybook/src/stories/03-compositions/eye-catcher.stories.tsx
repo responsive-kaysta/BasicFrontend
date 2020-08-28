165f0f95625043dbed28b122e51aa6f071c838ad
Kay Stuckenschmidt <kay.stuckenschmidt@subsidia.ch>
2020-08-28 23:05:50 +0200

    Eye-Catcher

--- packages/basic-frontend-storybook/src/stories/03-compositions/eye-catcher.stories.tsx
+++ packages/basic-frontend-storybook/src/stories/03-compositions/eye-catcher.stories.tsx
@@ -1,26 +1,56 @@
+import { text } from "@storybook/addon-knobs";
 import { storiesOf } from "@storybook/react";
-import { IntroEyeCatcher, SimpleEyeCatcher } from "basic-frontend-common";
+import {
+  IntroEyeCatcher,
+  SimpleEyeCatcher,
+  ThemeGray,
+} from "basic-frontend-common";
 import React from "react";
 
-storiesOf("Design System / 03 Compositions", module).add("Eye-Catcher", () => (
-  <div>
-    <section className="flex flex-row mb-10">
-      <div className="w-1/4 text-xl">Type</div>
-      <div className="w-3/4 text-xl">Swarm</div>
-    </section>
+storiesOf("Design System / 03 Compositions / Eye-Catcher", module)
+  .add("SimpleEyeCatcher", () => (
+    <div>
+      <section className="flex flex-row mb-10">
+        <div className="w-1/4 text-xl">Type</div>
+        <div className="w-3/4 text-xl">Swarm</div>
+      </section>
 
-    <section className="flex flex-row mb-10">
-      <div className="w-1/4">SimpleEyeCatcher</div>
-      <div className="w-3/4 flex flex-row justify-between">
-        <SimpleEyeCatcher contentTop="Top" contentBottom="Bottom" />
-      </div>
-    </section>
+      <section className="flex flex-row mb-10">
+        <div className="w-1/4">SimpleEyeCatcher</div>
+        <div className="w-3/4 flex flex-row justify-between">
+          <SimpleEyeCatcher
+            contentTop={text("top", "Lorem ipsum dolor sit amet")}
+            contentBottom={text(
+              "bottom",
+              "At vero eos et accusam et justo duo dolores et ea rebum"
+            )}
+            theme={ThemeGray}
+            cssStyle={text("cssStyle", "py-24")}
+          />
+        </div>
+      </section>
+    </div>
+  ))
+  .add("IntroEyeCatcher", () => (
+    <div>
+      <section className="flex flex-row mb-10">
+        <div className="w-1/4 text-xl">Type</div>
+        <div className="w-3/4 text-xl">Swarm</div>
+      </section>
 
-    <section className="flex flex-row mb-10">
-      <div className="w-1/4">IntroEyeCatcher</div>
-      <div className="w-3/4 flex flex-row justify-between">
-        <IntroEyeCatcher contentTop="Top" contentBottom="Bottom" />
-      </div>
-    </section>
-  </div>
-));
+      <section className="flex flex-row mb-10">
+        <div className="w-1/4">IntroEyeCatcher</div>
+        <div className="w-3/4 flex flex-row justify-between">
+          <IntroEyeCatcher
+            contentTop={text("top", "Lorem ipsum dolor sit amet")}
+            contentBottom={text(
+              "bottom",
+              "At vero eos et accusam et justo duo dolores et ea rebum"
+            )}
+            theme={ThemeGray}
+            cssStyle={text("cssStyle", "py-24")}
+          />
+        </div>
+      </section>
+    </div>
+  ));
