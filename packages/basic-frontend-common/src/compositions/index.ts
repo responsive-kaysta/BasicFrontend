8b11254f581413f414f5e4596f9503f81bd4c605
Kay Stuckenschmidt <kay.stuckenschmidt@subsidia.ch>
2020-10-19 22:50:32 +0200

    react-pwa-toolbox

--- packages/basic-frontend-common/src/compositions/index.ts
+++ packages/basic-frontend-common/src/compositions/index.ts
@@ -3,5 +3,6 @@ export * from './cookie-banner';
 export * from './eye-catcher';
 export * from './image-captcha';
 export * from './panels';
+export * from './pwa-update-banner';
 export * from './sections';
 export * from './title-blocks';
