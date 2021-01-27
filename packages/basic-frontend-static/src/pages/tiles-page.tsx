55cd0f6904b3f071bfa8e47d0f8feab9a8b280da
Kay Stuckenschmidt <kay.stuckenschmidt@subsidia.ch>
2021-01-27 11:56:16 +0100

    tiles spacing, version increment

--- packages/basic-frontend-static/src/pages/tiles-page.tsx
+++ packages/basic-frontend-static/src/pages/tiles-page.tsx
@@ -5,7 +5,6 @@ import {
   IntroSimple,
   Spacer,
   SubTitle,
-  ThemeLight,
   ThemeTransparent,
   TileItem,
   TileSection,
@@ -29,73 +28,73 @@ class TilesPage extends React.Component {
               />
 
               <ContainerCover theme={ThemeTransparent}>
-                <ContainerSection theme={ThemeTransparent}>
+                <ContainerSection theme={ThemeTransparent} marginBottom>
                   <SubTitle
                     theme={ThemeTransparent}
                     title="Little Effort, maximum Result"
                     leadText="Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet"
                   />
                 </ContainerSection>
-
-                <TileSection theme={ThemeTransparent}>
-                  <TileItem
-                    headline="Product Page"
-                    date="Aug. 1. 2020"
-                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
-                    link="/product-page"
-                    bgImage="/backgrounds/team.png"
-                    theme={ThemeTransparent}
-                  />
-
-                  <TileItem
-                    headline="Contact Page"
-                    date="Aug. 1. 2020"
-                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
-                    link="/contact-page"
-                    bgImage="/backgrounds/internet_1.png"
-                    theme={ThemeTransparent}
-                  />
-
-                  <TileItem
-                    headline="Links Page"
-                    date="Aug. 1. 2020"
-                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
-                    link="/links-page"
-                    bgImage="/backgrounds/settings-gears.png"
-                    theme={ThemeTransparent}
-                  />
-
-                  <TileItem
-                    headline="Legal Page"
-                    date="Aug. 1. 2020"
-                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
-                    link="/legal-page"
-                    bgImage="/backgrounds/hosting.png"
-                    theme={ThemeTransparent}
-                  />
-
-                  <TileItem
-                    headline="Tiles Page"
-                    date="Aug. 1. 2020"
-                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
-                    link="/tiles-page"
-                    bgImage="/backgrounds/engineer.png"
-                    theme={ThemeTransparent}
-                  />
-
-                  <TileItem
-                    headline="About Page"
-                    date="Aug. 1. 2020"
-                    description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
-                    link="/about-page"
-                    bgImage="/backgrounds/planning.png"
-                    theme={ThemeTransparent}
-                  />
-                </TileSection>
-
-                <Spacer />
-                <ToTopLink theme={ThemeLight} />
               </ContainerCover>
+
+              <TileSection theme={ThemeTransparent}>
+                <TileItem
+                  headline="Product Page"
+                  date="Aug. 1. 2020"
+                  description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
+                  link="/product-page"
+                  bgImage="https://cdn.responsive-it.biz/assets/bg-images/team.png"
+                  theme={ThemeTransparent}
+                />
+
+                <TileItem
+                  headline="Contact Page"
+                  date="Aug. 1. 2020"
+                  description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
+                  link="/contact-page"
+                  bgImage="https://cdn.responsive-it.biz/assets/bg-images/internet_1.png"
+                  theme={ThemeTransparent}
+                />
+
+                <TileItem
+                  headline="Links Page"
+                  date="Aug. 1. 2020"
+                  description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
+                  link="/links-page"
+                  bgImage="https://cdn.responsive-it.biz/assets/bg-images/settings-gears.png"
+                  theme={ThemeTransparent}
+                />
+
+                <TileItem
+                  headline="Legal Page"
+                  date="Aug. 1. 2020"
+                  description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
+                  link="/legal-page"
+                  bgImage="https://cdn.responsive-it.biz/assets/bg-images/hosting.png"
+                  theme={ThemeTransparent}
+                />
+
+                <TileItem
+                  headline="Tiles Page"
+                  date="Aug. 1. 2020"
+                  description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
+                  link="/tiles-page"
+                  bgImage="https://cdn.responsive-it.biz/assets/bg-images/engineer.png"
+                  theme={ThemeTransparent}
+                />
+
+                <TileItem
+                  headline="About Page"
+                  date="Aug. 1. 2020"
+                  description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr"
+                  link="/about-page"
+                  bgImage="https://cdn.responsive-it.biz/assets/bg-images/planning.png"
+                  theme={ThemeTransparent}
+                />
+              </TileSection>
+
+              <Spacer />
+              <ToTopLink />
             </ContainerArticle>
           </>
         ) : null}
